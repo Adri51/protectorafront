f94daba86280dac94c3bfc4480f27af3949c00bb
adri51 <adriibm91@gmail.com>
2019-11-25 17:36:30 +0100

    cabecera y footer

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PerrosService } from '../perros.service';
+import { PerrerasService } from '../perreras.service';
 
 @Component({
   selector: 'app-principal',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  buscador: FormGroup;
+
+  constructor(private perrosService: PerrosService, private perrerasService: PerrerasService) {
+    this.buscador = new FormGroup({
+      perrera: new FormControl('', [
+        Validators.required
+      ]),
+      raza: new FormControl('', [
+        Validators.required
+      ]),
+      color: new FormControl(''),
+      tamaño: new FormControl(''),
+      edad: new FormControl('')
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.buscador.value);
   }
 
 }
